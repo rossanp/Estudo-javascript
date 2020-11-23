7c0abb0e8eba7a1c0abc970cebc04d95009e1d4a
Rossan P <53863541+rossanp@users.noreply.github.com>
2020-11-23 16:27:07 -0300

    alterações

--- Calculadora/javascript.js
+++ Calculadora/javascript.js
@@ -2,26 +2,26 @@ function calc(){
   let valor1 = document.getElementById('valor-1').value;
   let valor2 = document.getElementById('valor-2').value;
   let sinal = document.getElementById('sinal').value;
-  let resultado = parseInt(valor1)+parseInt(valor2);
+  let resultado;
   //document.write(sinal);
     
   switch (sinal) {
   case "soma":
-    document.getElementById("result").innerHTML = "A soma é " + resultado + ".";
+    document.getElementById("result").innerHTML = "A soma é " + (resultado = parseInt(valor1)+parseInt(valor2)) + ".";
     break;
   case "sub":
-    day = "Monday";
+    document.getElementById("result").innerHTML = "A subtração é " + (resultado = parseInt(valor1)-parseInt(valor2)) + ".";
     break;
   case "mult":
-     day = "Tuesday";
+     document.getElementById("result").innerHTML = "A multiplicação é " + (resultado = parseInt(valor1)*parseInt(valor2)) + ".";
     break;
   case "div":
-    day = "Wednesday";
+    document.getElementById("result").innerHTML = "A divisão é " + (resultado = parseInt(valor1)/parseInt(valor2)) + ".";
     break;
   case "porc":
-    day = "Thursday";
+    document.getElementById("result").innerHTML = "A porcentagem é " + (resultado = (parseInt(valor1)/ 100) * parseInt(valor2)) + ".";
     break;
   default:
-    day = "Friday";
+    document.getElementById("result").innerHTML = "Valores incorretos.";
   }
 }
